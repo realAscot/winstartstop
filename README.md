6405c0c0464e91ecbb429a978b6dcee8bf75d35b
Adam Skotarczak <adam@skotarczak.net>
2025-04-18 22:37:43 +0200

    Überarbeitung und Verbesserung

--- README.md
+++ README.md
@@ -1,20 +1,34 @@
 # Sammulung von Start- Shutdown Scripten
 
-(C) 2025 Adam Skotarczak
+(C) 2025 Adam Skotarczak <adam@skotarczak.net> ([LICENZE](./LIZENSE))
 
-## Skritpe
+## Skripte
 
 [PLATZHALTER]
 
-### **[shutdown.bat](./scripts/shutdown.bat)**
+### [`shutdown.bat`](./scripts/shutdown.bat)
 
 Löscht diverse tempoäre Pfade unter windows. Sollte z.B beim Shutdown eingesetzt werden:
 
+#### Installation
+
 `gpedit.msc → Computerkonfiguration → Windows-Einstellungen → Skripts (Herunterfahren)`  
+
+![Screesnshot gpedit.msc](./assets/gpedit.png)  
+
+#### Parameter
 
 | Aufruf                      | Wirkung                                              |  
 |-----------------------------|------------------------------------------------------|  
 | `shutdown.bat`              | Führe Cleanup aus, keine Logs                        |  
 | `shutdown.bat debug`        | Führe Cleanup aus + logge nach shutdownbat.log       |  
 | `shutdown.bat test`         | Simuliere nur, keine Daten werden gelöscht           |  
-| `shutdown.bat test debu1g`  | Simuliere + logge alles                              |  
+| `shutdown.bat test debug`   | Simuliere + logge alles                              |  
+
+### [`startup.bat`](./scripts/startup.bat)
+
+Erstellt an einem beliebigen Pfad eine Verzeichnisebene.  
+
+> `./startup.bat R:\`
+
+Das Skript erstellt beim ersten Aufruf die Datei `dirlist.txt` in gleichen Veerzeichnis in dem sich das Skript befindet. Diese Datei kann bearbeitet und weitere Verzeichnise hinzugefügt werden. Mit erneutem Aufruf werden diese Verzeichnise erstellt.
--- scripts/startup.bat
+++ scripts/startup.bat
@@ -1,19 +1,53 @@
 @echo off
+setlocal enabledelayedexpansion
 
 ::
-:: Adam Skotarczak 2025
-:: Skript legt Verzeichnisse beim Bootvorgang in der Ramdisk an
+:: (C) Adam Skotarczak 2025
+:: Erstellt Ramdisk-Verzeichnisse – Pfad als Parameter übergeben
 ::
 
-:: Tempverzeichnis in Ramdoisk erstellen
-if not exist R:\Temp mkdir R:\Temp
+:: --- Konfiguration: Verzeichnisliste ---
+:: (Kommentare mit :: möglich, Leerzeilen erlaubt)
+set "DIRS_FILE=%~dp0dirlist.txt"
 
-:: Verzeichnisse für Chrome anlegen in Ramdisk
-if not exist R:\ChromeProfile mkdir R:\ChromeProfile
-if not exist R:\ChromeCache mkdir R:\ChromeCache
+:: Wenn dirlist.txt nicht existiert, erzeugen
+if not exist "%DIRS_FILE%" (
+    >"%DIRS_FILE%" (
+        echo :: Verzeichnisse, die in der Ramdisk erstellt werden sollen
+        echo Temp
+        echo ChromeProfile
+        echo ChromeCache
+        echo Downloads
+        echo Arbeitsverzeichnis
+    )
+    echo Datei dirlist.txt wurde erzeugt. Inhalt ggf. anpassen und erneut ausfuehren.
+    goto :eof
+)
 
-:: Arbeitsverzeichnis anlegen
-if not exist R:\Arbeitsverzeichnis mkdir R:\Arbeitsverzeichnis
+:: --- Hilfe anzeigen, wenn kein Parameter übergeben wird ---
+if "%~1"=="" (
+    echo.
+    echo Syntax: %~nx0 [Laufwerkspfad]
+    echo Beispiel: %~nx0 R:\
+    echo Erstellt folgende Verzeichnisse in der Ramdisk:
+    for /f "usebackq tokens=*" %%D in ("%DIRS_FILE%") do (
+        set "LINE=%%D"
+        if not "!LINE!"=="" if "!LINE:~0,2!" NEQ "::" echo   - !LINE!
+    )
+    goto :eof
+)
 
-echo 1
-:: END
+:: --- Zielpfad aus Parameter setzen ---
+set "RAMDISK=%~1"
+if not "%RAMDISK:~-1%"=="\" set "RAMDISK=%RAMDISK%\"
+
+:: --- Verzeichnisse erstellen ---
+for /f "usebackq tokens=*" %%D in ("%DIRS_FILE%") do (
+    set "LINE=%%D"
+    if not "!LINE!"=="" if "!LINE:~0,2!" NEQ "::" (
+        if not exist "%RAMDISK%!LINE!" mkdir "%RAMDISK%!LINE!"
+    )
+)
+
+echo Verzeichnisse in %RAMDISK% wurden erstellt.
+endlocal
